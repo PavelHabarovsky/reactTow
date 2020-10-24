e6145c9c155d45f88b29d28e835417f1b5f3fda6
Pavel Habarovsky <silich2@mail.ru>
2020-10-24 19:07:43 +0600

    вторя фигня

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from "./components/Header"
 import Navbar from "./components/Navbar"
-import Content from './components/Content';
+import Profile from './components/Profile';
 
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
     <div className="app-wrapper">
       <Header/>
       <Navbar/>
-      <Content/>
+      <Profile/>
      
 
 
