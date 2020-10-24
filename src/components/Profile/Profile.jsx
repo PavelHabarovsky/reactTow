99c0b1b393ca2e5df7c2945c8700756d29c98b37
Pavel Habarovsky <silich2@mail.ru>
2020-10-24 20:09:20 +0600

    Первый нормальный commit

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import s from "./Profile.module.css"
+
+const Profile = () => {
+  return (
+    <div className={s.profile}>
+      <div>
+        <div />
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRntxVMoJQb7AkEvU3VTAaBJL96OCHpiX3wHA&usqp=CAU" /></div>
+
+    </div>
+  )
+}
+export default Profile;
